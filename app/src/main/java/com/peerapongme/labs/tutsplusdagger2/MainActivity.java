0d618d8be7b54e0b7dabb09cb4f14bbef2f4b909
Peerapong Samarnpong <peerapong_sam@hotmail.com>
2016-08-19 15:04:49 +0700

    Step 4: Using @Component Interface to Obtain Objects

--- app/src/main/java/com/peerapongme/labs/tutsplusdagger2/MainActivity.java
+++ app/src/main/java/com/peerapongme/labs/tutsplusdagger2/MainActivity.java
@@ -1,13 +1,28 @@
 package com.peerapongme.labs.tutsplusdagger2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.peerapongme.labs.tutsplusdagger2.components.DaggerVehicleComponent;
+import com.peerapongme.labs.tutsplusdagger2.components.VehicleComponent;
+import com.peerapongme.labs.tutsplusdagger2.models.Vehicle;
+import com.peerapongme.labs.tutsplusdagger2.modules.VehicleModule;
 
 public class MainActivity extends AppCompatActivity {
+
+    Vehicle mVehicle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        VehicleComponent vehicleComponent = DaggerVehicleComponent.builder().vehicleModule(new VehicleModule()).build();
+
+        mVehicle = vehicleComponent.provideVehicle();
+
+        Toast.makeText(this, String.valueOf(mVehicle.getSpeed()), Toast.LENGTH_SHORT).show();
     }
 }
